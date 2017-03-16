a626a7cdad2b775f527450d5b5d2b612edced993
Pavel Egorov <snake7799@users.noreply.github.com>
2017-03-16 21:52:06 +0300

    js-assignments/feature: initial commit

---
+++ task/07-yield-tasks.js
@@ -0,0 +1,197 @@
+'use strict';
+
+/********************************************************************************************
+ *                                                                                          *
+ * Plese read the following tutorial before implementing tasks:                             *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators   *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/yield        *
+ *                                                                                          *
+ ********************************************************************************************/
+
+
+/**
+ * Returns the lines sequence of "99 Bottles of Beer" song:
+ *
+ *  '99 bottles of beer on the wall, 99 bottles of beer.'
+ *  'Take one down and pass it around, 98 bottles of beer on the wall.'
+ *  '98 bottles of beer on the wall, 98 bottles of beer.'
+ *  'Take one down and pass it around, 97 bottles of beer on the wall.'
+ *  ...
+ *  '1 bottle of beer on the wall, 1 bottle of beer.'
+ *  'Take one down and pass it around, no more bottles of beer on the wall.'
+ *  'No more bottles of beer on the wall, no more bottles of beer.'
+ *  'Go to the store and buy some more, 99 bottles of beer on the wall.'
+ *
+ * See the full text at
+ * http://99-bottles-of-beer.net/lyrics.html
+ *
+ * NOTE: Please try to complete this task faster then original song finished:
+ * https://www.youtube.com/watch?v=Z7bmyjxJuVY   :)
+ *
+ *
+ * @return {Iterable.<string>}
+ *
+ */
+function* get99BottlesOfBeer() {
+	let count = 99;
+	while (count >= 0) {
+		if (count >= 2) {
+			yield(`${count} bottles of beer on the wall, ${count} bottles of beer.`);
+			yield(`Take one down and pass it around, ${count - 1} bottle${count - 1 !== 1 ? 's' : ''} of beer on the wall.`);
+		} else if (count === 1) {
+			yield(`${count} bottle of beer on the wall, ${count} bottle of beer.`);
+			yield("Take one down and pass it around, no more bottles of beer on the wall.");
+		} else {
+			yield("No more bottles of beer on the wall, no more bottles of beer.");
+			yield("Go to the store and buy some more, 99 bottles of beer on the wall.");
+		}
+		count--;
+	}
+}
+
+
+/**
+ * Returns the Fibonacci sequence:
+ *   0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+ *
+ * See more at: https://en.wikipedia.org/wiki/Fibonacci_number
+ *
+ * @return {Iterable.<number>}
+ *
+ */
+function* getFibonacciSequence() {
+	var current = 1;
+	var a = 1;
+	var b = 1;
+	
+	yield 0;
+	yield 1;
+	
+	while (true) {
+		current = b;
+		yield current;
+		b = a + b;
+		a = current;
+	}
+}
+
+
+/**
+ * Traverses a tree using the depth-first strategy
+ * See details: https://en.wikipedia.org/wiki/Depth-first_search
+ *
+ * Each node have child nodes in node.children array.
+ * The leaf nodes do not have 'children' property.
+ *
+ * @params {object} root the tree root
+ * @return {Iterable.<object>} the sequence of all tree nodes in depth-first order
+ * @example
+ *
+ *   var node1 = { n:1 }, node2 = { n:2 }, node3 = { n:3 }, node4 = { n:4 },
+ *       node5 = { n:5 }, node6 = { n:6 }, node7 = { n:7 }, node8 = { n:8 };
+ *   node1.children = [ node2, node6, node7 ];
+ *   node2.children = [ node3, node4 ];
+ *   node4.children = [ node5 ];
+ *   node7.children = [ node8 ];
+ *
+ *     source tree (root = 1):
+ *            1
+ *          / | \
+ *         2  6  7
+ *        / \     \            =>    { 1, 2, 3, 4, 5, 6, 7, 8 }
+ *       3   4     8
+ *           |
+ *           5
+ *
+ *  depthTraversalTree(node1) => node1, node2, node3, node4, node5, node6, node7, node8
+ *
+ */
+function* depthTraversalTree(root) {
+	let stack = [root];
+	let currentNode;
+	
+	while (stack.length > 0) {
+		currentNode = stack.pop();
+		yield currentNode;
+		
+		if (currentNode.children) {
+			currentNode.children.reverse();
+			currentNode.children.forEach(node => stack.push(node));
+		}
+	}
+}
+
+
+/**
+ * Traverses a tree using the breadth-first strategy
+ * See details: https://en.wikipedia.org/wiki/Breadth-first_search
+ *
+ * Each node have child nodes in node.children array.
+ * The leaf nodes do not have 'children' property.
+ *
+ * @params {object} root the tree root
+ * @return {Iterable.<object>} the sequence of all tree nodes in breadth-first order
+ * @example
+ *     source tree (root = 1):
+ *
+ *            1
+ *          / | \
+ *         2  3  4
+ *        / \     \            =>    { 1, 2, 3, 4, 5, 6, 7, 8 }
+ *       5   6     7
+ *           |
+ *           8
+ *
+ */
+function* breadthTraversalTree(root) {
+	let queue = [root];
+	for (let key of queue) {
+		yield key;
+		if (key.children)
+			queue.push(...key.children);
+	}
+}
+
+
+/**
+ * Merges two yield-style sorted sequences into the one sorted sequence.
+ * The result sequence consists of sorted items from source iterators.
+ *
+ * @params {Iterable.<number>} source1
+ * @params {Iterable.<number>} source2
+ * @return {Iterable.<number>} the merged sorted sequence
+ *
+ * @example
+ *   [ 1, 3, 5, ... ], [2, 4, 6, ... ]  => [ 1, 2, 3, 4, 5, 6, ... ]
+ *   [ 0 ], [ 2, 4, 6, ... ]  => [ 0, 2, 4, 6, ... ]
+ *   [ 1, 3, 5, ... ], [ -1 ] => [ -1, 1, 3, 5, ...]
+ */
+function* mergeSortedSequences(source1, source2) {
+	let a = source1();
+	let b = source2();
+	let x;
+	let y;
+	
+	while (true) {
+		x = a.next().value;
+		y = b.next().value;
+
+		if (x == null)
+			yield y;
+		else if (y == undefined)
+			yield x;
+		else {
+			yield Math.min(x,y);
+			yield Math.max(x,y);
+		}
+	}
+}
+
+
+module.exports = {
+    get99BottlesOfBeer: get99BottlesOfBeer,
+    getFibonacciSequence: getFibonacciSequence,
+    depthTraversalTree: depthTraversalTree,
+    breadthTraversalTree: breadthTraversalTree,
+    mergeSortedSequences: mergeSortedSequences
+};
